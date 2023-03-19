b6cd387f2e9c057b87e6a92645e29562ef5aaee6
Noor Mohamed <118723182+Noormohamed28@users.noreply.github.com>
2023-03-19 17:17:22 +0530

    Add files via upload

---
+++ src/data/index.js
@@ -0,0 +1,21 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Noor',
+    lastName: 'Mohamed',
+    email: 'noormohamed28@gmail.com',
+    salary: '200000',
+    date: '19-03-2023'
+  },
+  {
+    id: 2,
+    firstName: 'Sameem',
+    lastName: 'Sha',
+    email: 'sameeem@hotmail.com',
+    salary: '100000',
+    date: '16-09-2022'
+  }
+
+];
+
+export { employeesData };
